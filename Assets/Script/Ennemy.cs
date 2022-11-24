c76c169fa0895c1898b4b702c7ad6a284052d6c2
Aurelien Tavernier <aurelientav@gmail.com>
2022-11-24 16:27:42 +0100

    Ennemy moves update, can chase the player

--- Assets/Script/Ennemy.cs
+++ Assets/Script/Ennemy.cs
@@ -4,27 +4,66 @@ using UnityEngine;
 
 public class Ennemy : MonoBehaviour
 {
-    Rigidbody2D rb = null;
+    private Transform targetPlayer;
+    private Transform EnnemyTransform;
+    private Rigidbody2D rb = null;
     public int Hp;
     public float speed = 0;
+    private bool hole = false;
+    private bool wall = false;
     SpriteRenderer renderer = null;
     // Start is called before the first frame update
     void Start()
     {
+        EnnemyTransform = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(-1 * speed, 0);
+        if(targetPlayer == null)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+        if ((targetPlayer.position.x < EnnemyTransform.position.x) && (targetPlayer.position.x - EnnemyTransform.position.x < 5000))
+        {
+            ChasePlayer();
+        }
+        if ((targetPlayer.position.x > EnnemyTransform.position.x) && (EnnemyTransform.position.x - targetPlayer.position.x < 5000))
+        {
+            ChasePlayer();
+        }
+        rb.velocity = new Vector2(speed, 0);
         if (-1 * speed != 0)
         {
-            renderer.flipX = -1 * speed < 0;
+            renderer.flipX =  speed < 0;
         }
     }
-
+    void ChasePlayer()
+    {
+        speed = 15;
+        if (targetPlayer.position.x < EnnemyTransform.position.x)
+        {
+            if (speed > 0)
+            {
+                speed = speed * -1;
+            }
+        }
+        else
+        {
+            if (speed < 0)
+            {
+                speed = speed * -1;
+            }
+        }
+        if (hole || wall)
+        {
+            rb.AddForce(new Vector2(0, 15), ForceMode2D.Impulse);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Projectile")
@@ -39,15 +78,21 @@ public class Ennemy : MonoBehaviour
         }
         else
         {
-            Debug.Log("yay");
             speed = speed * -1;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.GetContact(0).normal.x < 0)
+        Debug.Log(collision.GetContact(0).normal.x);
+        if (collision.GetContact(0).normal.x > 0.5)
         {
-            Debug.Log(collision.GetContact(0).normal.y);
+            
+            speed = speed * -1;
+            renderer.flipX = -1 * speed < 0;
+        }
+        if (collision.GetContact(0).normal.x < -0.5)
+        {
+
             speed = speed * -1;
             renderer.flipX = -1 * speed < 0;
         }
