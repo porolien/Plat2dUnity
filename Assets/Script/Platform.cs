cc2d318fd1805c7f97d4350942eddd32ecbab601
titouan garnier <garniertitouan4@gmail.com>
2022-11-30 10:16:17 +0100

    Plateform qui tombe

---
+++ Assets/Script/Platform.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float fallDelay = 1f;
+
+
+    private Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+}
--- Assets/Script/Tir.cs
+++ Assets/Script/Tir.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class Tir : MonoBehaviour
 {
     Rigidbody rb;
+    public int H;
+    public int L;
+    [SerializeField] private bool bullet;
+
+
     public void Start()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(10, 15), ForceMode2D.Impulse);
+        rb.AddForce(new Vector2(H, L), ForceMode2D.Impulse);
+        
     }
 
     private void Update()
