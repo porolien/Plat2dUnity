a8ca2b30dddc350f34ea52244616ca1a62828415
titouan garnier <garniertitouan4@gmail.com>
2022-12-06 22:41:23 +0100

    Partie oublier de l'EnnemyShoot

---
+++ Assets/Script/Tir.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tir : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {          
+                Destroy(gameObject);
+        }
+    }
+}
