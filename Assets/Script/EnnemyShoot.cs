669cd11c50d9cead4899bc94cd45c4a55de388c5
titouan garnier <garniertitouan4@gmail.com>
2022-12-16 08:31:22 +0100

    Modification MainMenu

--- Assets/Script/EnnemyShoot.cs
+++ Assets/Script/EnnemyShoot.cs
@@ -2,109 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnnemyShoot : Ennemy
+public class EnnemyShoot : MonoBehaviour
 {
 
-    Transform EnnemyTransform;
-    public Transform ProjectilePosition;
+
+    public int Hp;
+
+    private Transform targetPlayer;
+    private Transform EnnemyTransform;
+    private Rigidbody2D rb = null;
+    public float speed = 0;
+    private bool hole = false;
+    private bool wall = false;
+    private bool ChasePlayer = false;
+    private bool WalkOnWall = false;
+    SpriteRenderer renderer = null;
     public float AtkSpeedProjectile;
     public GameObject Projectile;
-    private Transform targetPlayer;
-    public float ymax = 15;
-    private bool attackWaiting = false;
+    public Transform ProjectilePosition;
+    public int direction;
+    private bool Tir = false; 
+
     void Start()
     {
-        EnnemyTransform = GetComponent<Transform>();
-        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-
-
+        
     }
 
 
     void Update()
     {
+        GameObject newProjectile = Instantiate(Projectile, new Vector2(ProjectilePosition.position.x, ProjectilePosition.position.y), Quaternion.identity);
+        StartCoroutine(ChangeProjectileDirection(newProjectile, direction));
 
-        int checkX = 0;
-        int checkY = 0;
-        if ((targetPlayer.position.x < EnnemyTransform.position.x) && (targetPlayer.position.x - EnnemyTransform.position.x > -ymax)) 
-        {   
-            checkX = -1;
-        }
-        if ((targetPlayer.position.x > EnnemyTransform.position.x) && (EnnemyTransform.position.x - targetPlayer.position.x > -ymax))
-        {
-            checkX = 1;
-        }
-        if ((targetPlayer.position.y < EnnemyTransform.position.y) && (targetPlayer.position.y - EnnemyTransform.position.y > -ymax))
-        {
-            checkY = 1;
-        }
-        else if ((targetPlayer.position.y > EnnemyTransform.position.y) && (EnnemyTransform.position.y - targetPlayer.position.y > -ymax))
-        {
-            checkY = -1;
-        }
-        if (checkX != 0 && checkY != 0 && attackWaiting == false)
-        {
-            Attack(checkX, checkY);
-        }
-        
+        StartCoroutine(Shoot());
       
 
     }
 
 
-
-    
-
-    private void Attack(int checkX, int checkY)
+    IEnumerator Shoot()
     {
-        GameObject newProjectile = Instantiate(Projectile, new Vector2(ProjectilePosition.position.x, ProjectilePosition.position.y), Quaternion.identity);
-        StartCoroutine(MortarProjectile(newProjectile, checkX, checkY));
-        
-        
-    }
-    private IEnumerator MortarProjectile(GameObject Projectile, int Direction, int checkY)
-    {
-        attackWaiting = true;
-        float DifferenceYPosition = targetPlayer.position.y - EnnemyTransform.position.y;
-        float DifferenceXPosition = targetPlayer.position.x - EnnemyTransform.position.x;
-        float DifferenceYPlayerEnnemy = DifferenceYPosition / ymax; 
-        float Range = (DifferenceYPlayerEnnemy * 0.33f) +1;
-        float attackDuration = ((targetPlayer.position.x - EnnemyTransform.position.x) / 5) + 1;
-        if(Direction < 0)
+        if (((targetPlayer.position.x < EnnemyTransform.position.x) && (targetPlayer.position.x - EnnemyTransform.position.x < -10)) || ((targetPlayer.position.x > EnnemyTransform.position.x) && (EnnemyTransform.position.x - targetPlayer.position.x < -10)))
         {
-            attackDuration = ((EnnemyTransform.position.x - targetPlayer.position.x) / 5) + 1;
+            Tir = true;
+            Debug.Log("Ahhhh");
         }
-        float earlyDuration = (attackDuration / 6) * Range;
-        float lastDuration = 1.33f - earlyDuration;
-        float height = (25 + DifferenceYPosition ) / 2;
-        float earlySpeedY = height * (3 * earlyDuration);
-        float lastSpeedY = -(25 - height) * (3 * lastDuration);
-        float earlySpeedX = DifferenceXPosition / attackDuration * earlyDuration;
-        float lastSpeedX = DifferenceXPosition / attackDuration * lastDuration; 
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(earlySpeedX, earlySpeedY * 1.2f);
-        yield return new WaitForSeconds(earlyDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(earlySpeedX, earlySpeedY);
-        yield return new WaitForSeconds(earlyDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(earlySpeedX, earlySpeedY * 0.8f);
-        yield return new WaitForSeconds(earlyDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(lastSpeedX, lastSpeedY * 0.8f);
-        yield return new WaitForSeconds(lastDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(lastSpeedX, lastSpeedY);
-        yield return new WaitForSeconds(lastDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(lastSpeedX, lastSpeedY * 1.2f);
-        yield return new WaitForSeconds(lastDuration);
-        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
-        attackWaiting = false;
+
+        if (Tir = true)
+        {
+            yield return new WaitForSeconds(1.0f);
+        }
     }
+
+
+    private IEnumerator ChangeProjectileDirection(GameObject Projectile, int Direction)
+    {
+        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(AtkSpeedProjectile * Direction * Time.fixedDeltaTime, 5f);
+        Debug.Log(Projectile.transform.localScale.x * Direction + Projectile.transform.localScale.y + Projectile.transform.localScale.z);
+        Projectile.transform.rotation = Quaternion.Euler(0, 0, 45);
+        yield return new WaitForSeconds(0.4f);
+        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(AtkSpeedProjectile * Direction * Time.fixedDeltaTime, 0f);
+        Projectile.transform.rotation = Quaternion.Euler(0, 0, 18);
+        yield return new WaitForSeconds(0.05f);
+        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(AtkSpeedProjectile * Direction * Time.fixedDeltaTime, 0f);
+        Projectile.transform.rotation = Quaternion.Euler(0, 0, 0);
+        yield return new WaitForSeconds(0.05f);
+        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(AtkSpeedProjectile * Direction * Time.fixedDeltaTime, 0f);
+        Projectile.transform.rotation = Quaternion.Euler(0, 0, -18);
+        yield return new WaitForSeconds(0.05f);
+        Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(AtkSpeedProjectile * Direction * Time.fixedDeltaTime, -5f);
+        Projectile.transform.rotation = Quaternion.Euler(0, 0, -45);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Projectile")
+        if (collision.tag == "Player" || collision.tag == "Projectile")
         {
 
-            if (collision.tag == "PlayerProjectile")
+            Destroy(collision.gameObject);
+            Hp = Hp - 1;
+            if (Hp <= 0)
             {
-
-                TakeDamage(collision);
+                Destroy(gameObject);
             }
         }
     }
